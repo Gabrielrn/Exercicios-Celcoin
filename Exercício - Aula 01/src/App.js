24407c107384ba9392a2885bdbf9d149a156b169
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-10-24 14:06:51 -0300

    Exercício Café da Manhã - Aula 01

---
+++ Exercício - Aula 01/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="root"> 
+      <div>
+        <h1>Lista de Compras</h1>
+        <ul>
+          <li>Pão</li>
+          <li>Banana</li>
+          <li>Milk Shake de Doce de Leite</li>
+          <li>Nutella</li>
+          <li>Sorvete</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
